c77e36cf28ee9c6ee3db0e2fe099ef0bce0ae74b
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-05-06 09:43:51 -0300

    feat: includes answers for exercises 02 and 03 on the Ed.py file

--- Soluções/Ed.py
+++ Soluções/Ed.py
@@ -2,21 +2,26 @@
 #05/05/25
 #1. Crie um dicionário chamado aluno com as chaves "nome", "idade" e "nota". 
 #Atribua valores fictícios e imprima cada valor individualmente.
-
 aluno = {
   "nome": "Joao", 
   "idade": 20, 
   "nota": 8.5
 }
 
-print("Nome:", aluno["nome"])
-print("Idade:", aluno["idade"])
-print("Nota:", aluno["nota"]) 
+print(f"Nome: {aluno['nome']}" )
+print(f"Idade: {aluno["idade"]}")
+print(f"Nota: {aluno["nota"]}") 
 
 #2. Dado o dicionário abaixo:
-
-#dados = {"nome": "Carlos", "idade": 30, "curso": "Python"}
+dados = {"nome": "Carlos", "idade": 30, "curso": "Python"}
 #Altere o valor da chave "idade" para 31 e adicione uma nova chave "email" com qualquer e-mail fictício.
+dados["idade"] = 31
+dados["email"] = "carlos30@email.com"
+print(dados)
 
 #3. Escreva um código que pergunte ao usuário o nome de um produto e seu preço. 
 # Guarde essas informações em um dicionário e exiba-o no final.
+produto = {}
+produto["nome"] = input("Digite o nome do produto: ")
+produto["preco"] = float(input("Digite o preço do produto: "))
+print(produto)
